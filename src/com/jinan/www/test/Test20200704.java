2c7d2ae453d61e1c57531ad6a31cd8edc913d504
15066673970 <iamdepeng@163.com>
2020-07-06 18:57:52 +0800

    20200704小练习

---
+++ src/com/jinan/www/test/Test20200704.java
@@ -0,0 +1,10 @@
+package com.jinan.www.test;
+
+public class Test20200704 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+	}
+
+}
